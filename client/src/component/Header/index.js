9a09569878e280dc86291e7dd77501957f1d58aa
xuting <xuting@souche.com>
2018-04-06 22:15:32 +0800

    feat: 添加题干文字功能

--- client/src/component/Header/index.js
+++ client/src/component/Header/index.js
@@ -12,9 +12,9 @@ const HeaderComponent = () => (
             defaultSelectedKeys={['2']}
             style={{ lineHeight: '64px' }}
         >
-            <Menu.Item key="1">nav 1</Menu.Item>
-            <Menu.Item key="2">nav 2</Menu.Item>
-            <Menu.Item key="3">nav 3</Menu.Item>
+            <Menu.Item key="1">患者管理</Menu.Item>
+            <Menu.Item key="2">创建评定</Menu.Item>
+            <Menu.Item key="3">开始评定</Menu.Item>
         </Menu>
     </Header>
 );
--- client/src/pages/MakeSubject/Content.js
+++ client/src/pages/MakeSubject/Content.js
@@ -1,10 +1,77 @@
 import React, { Component } from 'react';
 
-import { Layout, Menu, Icon } from 'antd';
+import { Layout, Menu, Icon, Input, Upload } from 'antd';
 const { SubMenu } = Menu;
 const { Content, Sider } = Layout;
+const { TextArea }  = Input;
 
+const RenderRow = props => (
+    <div style={styles.renderRowContainer}>
+        <div style={styles.closeIcon} onClick={() => {props.delete(props.index);}}>
+            <Icon type="close"/>
+        </div>
+        {props.children}
+    </div>
+);
+
+/**
+ * Demo
+ * 题干：字、图、音
+ * 问题：文字选择题、键盘输入、画图输入选其一
+ * type: text/img/audio/video
+ * */
 class ContentBody extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            question: [], // {type: 'text', value: '今天是星期几'}, {type: 'img', value: 'http:\\img\logo.img'} ...
+            answer: {
+                options: [],
+                blankLength: false,
+                draw: false
+            }
+        }
+    }
+
+    _renderQuestion(){
+        let questionsRender = [];
+        this.state.question.map((item, index) => {
+            if (item) {
+                let renderRow = null;
+                switch (item.type){
+                    case 'text':
+                        renderRow =
+                            <RenderRow index={index} key={index} delete={this.delete}>
+                                <TextArea value={item.value} onChange={e => {
+                                    let state = this.state;
+                                    state.question[index].value = e.nativeEvent.target.value;
+                                    this.setState(state);
+                                }}/>
+                            </RenderRow>;
+                        questionsRender.push(renderRow);
+                        break;
+                    case 'img':
+                        renderRow =
+                            <RenderRow index={index} key={index} delete={this.delete}>
+                                <img key={index} src={item.value} alt={'error'}/>
+                            </RenderRow>;
+                        questionsRender.push(renderRow);
+                        break;
+                    case 'audio':
+                        renderRow =
+                            <RenderRow index={index} key={index} delete={this.delete}>
+                                <audio key={index} src={item.value}/>
+                            </RenderRow>;
+                        questionsRender.push(renderRow);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+        return questionsRender;
+    }
+
     render() {
         return (
             <Layout style={{ padding: '24px 0', background: '#fff' }}>
@@ -12,35 +79,70 @@ class ContentBody extends Component {
                     <Menu
                         mode="inline"
                         defaultSelectedKeys={['1']}
-                        defaultOpenKeys={['sub1']}
+                        defaultOpenKeys={['sub1', 'sub2']}
                         style={{ height: '100%' }}
+                        onClick={this._addContent}
                     >
-                        <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
-                            <Menu.Item key="1">option1</Menu.Item>
-                            <Menu.Item key="2">option2</Menu.Item>
-                            <Menu.Item key="3">option3</Menu.Item>
-                            <Menu.Item key="4">option4</Menu.Item>
+                        <SubMenu key="sub1" title={<span><Icon type="question-circle-o"/>创建题干</span>}>
+                            <Menu.Item key="addText">添加文字</Menu.Item>
+                            <Menu.Item key="addImg"><Upload>添加图片</Upload></Menu.Item>
+                            <Menu.Item key="3">添加音频</Menu.Item>
+                            <Menu.Item key="4">添加视频</Menu.Item>
                         </SubMenu>
-                        <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
-                            <Menu.Item key="5">option5</Menu.Item>
-                            <Menu.Item key="6">option6</Menu.Item>
-                            <Menu.Item key="7">option7</Menu.Item>
-                            <Menu.Item key="8">option8</Menu.Item>
-                        </SubMenu>
-                        <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
-                            <Menu.Item key="9">option9</Menu.Item>
-                            <Menu.Item key="10">option10</Menu.Item>
-                            <Menu.Item key="11">option11</Menu.Item>
-                            <Menu.Item key="12">option12</Menu.Item>
+                        <SubMenu key="sub2" title={<span><Icon type="plus-circle-o" />创建问题</span>}>
+                            <Menu.Item key="5">选择题</Menu.Item>
+                            <Menu.Item key="6">填空题</Menu.Item>
+                            <Menu.Item key="7">画图题</Menu.Item>
                         </SubMenu>
                     </Menu>
                 </Sider>
                 <Content style={{ padding: '0 24px', minHeight: 280 }}>
-                    Content
+                    {this._renderQuestion()}
                 </Content>
             </Layout>
         );
     }
+
+    /**
+     * 添加内容
+     * */
+    _addContent = ({ item, key, keyPath }) => {
+        switch (key){
+            case 'addText':
+                let state = this.state;
+                state.question.push({type: 'text', value: ''});
+                this.setState(state);
+                break;
+            case 'addImg':
+
+            default:
+                break;
+        }
+    };
+
+    /**
+     * 删除内容
+     * */
+    delete = (index) => {
+        let state = this.state;
+        state.question.splice(index, 1);
+        this.setState(state);
+    }
 }
+
+const styles = {
+    renderRowContainer: {
+        padding: 20,
+        backgroundColor: 'whitesmoke',
+        marginBottom: 20,
+        borderRadius: 4,
+        position: 'relative',   // 父元素设置 relative，子元素的 absolute 就相对于父元素
+    },
+    closeIcon: {
+        position: 'absolute',
+        top: 4,
+        right: 4,
+    }
+};
 
 export default ContentBody;
